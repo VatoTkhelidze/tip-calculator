238f9c6c056798f5beef4ee34c99191504030e72
VatoTkhelidze <vatotxelidze@gmail.com>
2022-07-26 06:23:44 +0400

    added first JS element on project

---
+++ tip-calculator.js
@@ -0,0 +1,13 @@
+
+
+percentClick = document.querySelector(".box-5")
+percentClick.addEventListener('click', function(){
+    percentClick.style.background = '#26C2AE'
+})
+
+function makeItLighter(){
+    percentClick.style.background = '#00474B'
+}
+
+
+
