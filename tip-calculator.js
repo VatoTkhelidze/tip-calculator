500a5e1be66d5cb69d98c344f8f4c95e6a7eac05
VatoTkhelidze <vatotxelidze@gmail.com>
2022-07-26 21:13:54 +0400

    added RESET button & hover and eventListener on percent boxes

--- tip-calculator.js
+++ tip-calculator.js
@@ -1,13 +1,32 @@
 
 
-percentClick = document.querySelector(".box-5")
-percentClick.addEventListener('click', function(){
-    percentClick.style.background = '#26C2AE'
-})
+percentClick = document.querySelectorAll(".percentButton")
 
-function makeItLighter(){
-    percentClick.style.background = '#00474B'
+numberClick = document.querySelectorAll(".percentColor")
+
+
+for(let i=0; i<percentClick.length; i++){
+    percentClick[i].addEventListener('click', function(){
+        for(let j=0; j<percentClick.length; j++){
+            percentClick[j].style.background = '#00474B'
+        }
+        percentClick[i].style.background = '#26C2AE';
+    })
+    
 }
+
+
+for(let i=0; i<numberClick.length; i++){
+    percentClick[i].addEventListener('click', function(){
+        for(let j=0; j<numberClick.length; j++){
+            numberClick[j].style.color = '#FFFFFF';
+        }
+        numberClick[i].style.color = '#00474B';
+    })
+}
+
+
+
 
 
 
