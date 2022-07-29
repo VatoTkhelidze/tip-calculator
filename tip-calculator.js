3b85dd7d40720474ceff43c1579229166e85f5c8
VatoTkhelidze <vatotxelidze@gmail.com>
2022-07-30 03:15:20 +0400

    created input values for 'Bill Amount'; 'Number of People' and added function for RESET button with JS

--- tip-calculator.js
+++ tip-calculator.js
@@ -5,6 +5,56 @@ percentClick = document.querySelectorAll(".percentButton")
 numberClick = document.querySelectorAll(".percentColor")
 
 
+
+
+billInput = document.querySelector(".money-amount input")
+billInput.addEventListener('input', function(){
+    console.log(billInput.value);
+})
+
+
+
+peopleNumberInput = document.querySelector(".people-amount input")
+peopleNumberInput.addEventListener('input', function(){
+    console.log(peopleNumberInput.value);
+})
+
+
+
+
+customButtonInput = document.querySelector(".custom-amount input")
+customButtonInput.addEventListener('input', function(){
+    console.log(customButtonInput.value);
+})
+
+
+
+
+
+
+
+tipAmountOutput = document.querySelector(".tip-amount-person")
+
+tipAmountOutput.addEventListener('output', function(){
+    console.log(tipAmountOutput.value);
+})
+
+
+
+
+
+resetButton = document.querySelector(".reset-box")
+resetButton.addEventListener('click', function(){
+    if(billInput.value || peopleNumberInput.value || customButtonInput.value > 0){
+        billInput.value = '';
+        peopleNumberInput.value = '';
+        customButtonInput.value = '';
+    }
+    
+})
+
+
+
 for(let i=0; i<percentClick.length; i++){
     percentClick[i].addEventListener('click', function(){
         for(let j=0; j<percentClick.length; j++){
@@ -26,6 +76,9 @@ for(let i=0; i<numberClick.length; i++){
 }
 
 
+
+
+//next step is to reset "percent box colors" when RESET button pressed.
 
 
 
