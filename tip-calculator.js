1f855f50095190873de14e2f77242d6b468ea026
VatoTkhelidze <vatotxelidze@gmail.com>
2022-08-02 03:41:00 +0400

    trying to make 'RESET' button fully functional, for example when its pressed the percent buttons color must be also reseted.(dark green)

--- tip-calculator.js
+++ tip-calculator.js
@@ -6,6 +6,32 @@ numberClick = document.querySelectorAll(".percentColor")
 
 
 
+for(let i=0; i<percentClick.length; i++){
+    percentClick[i].addEventListener('click', function(){
+        for(let j=0; j<percentClick.length; j++){
+            percentClick[j].style.background = '#00474B'
+        }
+        percentClick[i].style.background = '#26C2AE';
+    })
+    
+}
+
+
+for(let i=0; i<numberClick.length; i++){
+    percentClick[i].addEventListener('click', function(){
+        for(let j=0; j<numberClick.length; j++){
+            numberClick[j].style.color = '#FFFFFF';
+        }
+        numberClick[i].style.color = '#00474B';
+    })
+}
+
+
+
+
+
+
+
 
 
 
@@ -224,6 +250,7 @@ resetButton.addEventListener('click', function(){
         customButtonInput.value = '';
         tipAmountOutput.value = '$';
     }
+    percentClick.style.background = '#00474B';
 })
 
 //PERCENT BUTTONS
@@ -278,25 +305,7 @@ tipAmountOutput.addEventListener('output', function result(){
 
 
 
-for(let i=0; i<percentClick.length; i++){
-    percentClick[i].addEventListener('click', function(){
-        for(let j=0; j<percentClick.length; j++){
-            percentClick[j].style.background = '#00474B'
-        }
-        percentClick[i].style.background = '#26C2AE';
-    })
-    
-}
 
-
-for(let i=0; i<numberClick.length; i++){
-    percentClick[i].addEventListener('click', function(){
-        for(let j=0; j<numberClick.length; j++){
-            numberClick[j].style.color = '#FFFFFF';
-        }
-        numberClick[i].style.color = '#00474B';
-    })
-}
 
 
 
